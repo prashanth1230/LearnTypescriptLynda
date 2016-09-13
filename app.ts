d1b92dfbe1618c9d6716d7e27bfabe9a9a6d103c
prashanth1230 <prashanth1230@gmail.com>
2016-09-13 20:01:47 +0530

    Example of default parameters.

---
+++ app.ts
@@ -0,0 +1,12 @@
+var container = document.getElementById('container');
+
+function countDown(initial, final = 0, interval = 1) {
+    var current = initial;
+
+    while (current > final) {
+        container.innerHTML = current;
+        current -= interval;
+    }
+}
+
+countDown(10);
